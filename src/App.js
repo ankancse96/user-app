f56bb268330a2a14d6a8160c9dcddb8fc61e726b
ANKAN <ankancse96@gmail.com>
2021-02-28 14:35:06 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import logo from './logo.svg';
+import React,{useEffect, useState} from 'react';
 import './App.css';
 
 function App() {
+  const [user,setUser]=useState([]);
+  useEffect(()=>{
+    fetch('https://api.mocki.io/v1/640b8a36')
+    .then(res => res.json())
+    .then(data=>setUser(data))
+  },[])
   return (
     <div className="App">
+      <h1>user:{user.length}</h1>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
